8571d09e84d5b1dc70a251b3b51837cd40c49fd3
BimoChan07 <bmochanb@gmail.com>
2023-06-23 11:20:09 +0545

    User routes added

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { createUser } from "../controller/user.controller.js";
+import { createUser, updateUser } from "../controller/user.controller.js";
 
 const router = express.Router();
 router.use(express.json());
 router.post("/createUser", createUser);
+router.post("/updateUser", updateUser);
 
 export default router;
