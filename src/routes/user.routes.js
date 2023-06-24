a3286ca47fb0deea9ba288bfdb50217d07242090
BimoChan07 <bmochanb@gmail.com>
2023-06-24 09:20:32 +0545

    update and delete routes added

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,6 +4,7 @@ import { createUser, updateUser } from "../controller/user.controller.js";
 const router = express.Router();
 router.use(express.json());
 router.post("/createUser", createUser);
-router.post("/updateUser", updateUser);
+router.post("/updateUser/:id", updateUser);
+// router.post("/deleteUser/:id", deleteUser);
 
 export default router;
