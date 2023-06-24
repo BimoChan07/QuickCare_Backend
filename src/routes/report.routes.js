bbd7ee0d77ef30d4d7422ac2e7b1596d9ce59ec4
BimoChan07 <bmochanb@gmail.com>
2023-06-24 09:45:28 +0545

    update and delete routes added

--- src/routes/report.routes.js
+++ src/routes/report.routes.js
@@ -1,8 +1,16 @@
 import express from "express";
-import { createReport } from "../controller/report.controller.js";
+import {
+  createReport,
+  displayReport,
+  updateReport,
+  deleteReport,
+} from "../controller/report.controller.js";
 
 const router = express.Router();
 router.use(express.json());
 router.post("/createReport", createReport);
+router.post("/displayReport", displayReport);
+router.post("/updateReport", updateReport);
+router.post("/deleteReport", deleteReport);
 
 export default router;
