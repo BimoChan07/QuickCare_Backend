e1679c9efa973993d7a30d665460b9d5c5ea1279
BimoChan07 <bmochanb@gmail.com>
2023-06-24 09:22:54 +0545

    update and delete routes added

--- src/routes/doctor.routes.js
+++ src/routes/doctor.routes.js
@@ -1,8 +1,16 @@
 import express from "express";
-import { createDoctor } from "../controller/doctor.controller.js";
+import {
+  createDoctor,
+  displayDoctor,
+  updateDoctor,
+  deleteDoctor,
+} from "../controller/doctor.controller.js";
 
 const router = express.Router();
 router.use(express.json());
 router.post("/createDoctor", createDoctor);
+router.post("/displayDoctor/:id", displayDoctor);
+router.post("/updateDoctor/:id", updateDoctor);
+router.post("/deleteDoctor/:id", deleteDoctor);
 
 export default router;
