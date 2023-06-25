b406b869dfe99d842019d969206bf54d7d58c4d0
BimoChan07 <bmochanb@gmail.com>
2023-06-25 13:34:17 +0545

    update and delete routes added

--- src/routes/appointment.routes.js
+++ src/routes/appointment.routes.js
@@ -1,8 +1,16 @@
 import express from "express";
-import { createAppointment } from "../controller/appointment.controller.js";
+import {
+  createAppointment,
+  displayAppointment,
+  updateAppointment,
+  deleteAppointment,
+} from "../controller/appointment.controller.js";
 
 const router = express.Router();
 router.use(express.json());
 router.post("/createAppointment", createAppointment);
+router.post("/displayAppointment/:id", displayAppointment);
+router.post("/updateAppointment/:id", updateAppointment);
+router.post("/deleteAppointment/:id", deleteAppointment);
 
 export default router;
