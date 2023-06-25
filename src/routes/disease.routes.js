445a35fa93f911e4d669db96a6a58934f3615c6e
BimoChan07 <bmochanb@gmail.com>
2023-06-25 13:35:05 +0545

    update and delete routes added to disease

--- src/routes/disease.routes.js
+++ src/routes/disease.routes.js
@@ -1,8 +1,16 @@
 import express from "express";
-import { createDisease } from "../controller/disease.controller.js";
+import {
+  createDisease,
+  displayDisease,
+  updateDisease,
+  deleteDisease,
+} from "../controller/disease.controller.js";
 
 const router = express.Router();
 router.use(express.json());
 router.post("/createDisease", createDisease);
+router.post("/displayDisease/:id", displayDisease);
+router.post("/updateDisease/:id", updateDisease);
+router.post("/deleteDisease/:id", deleteDisease);
 
 export default router;
