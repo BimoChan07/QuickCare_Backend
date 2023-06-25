9f41a743d888bf450f9700261d1dc02cbfa5ef6d
BimoChan07 <bmochanb@gmail.com>
2023-06-25 13:32:30 +0545

    update and delete function added

--- src/controller/appointment.controller.js
+++ src/controller/appointment.controller.js
@@ -18,3 +18,53 @@ export const createAppointment = async (req, res) => {
       .json({ success: false, message: "Appointment creation failed" });
   }
 };
+
+// Display Appointment
+export const displayAppointment = async (req, res) => {
+  try {
+    const appointmentId = req.params.id;
+
+    const appointment = await Appointment.findById(appointmentId);
+    if (appointment) {
+      res.status(200).json(appointment);
+    } else {
+      res.status(200).json({ message: "Appointment deleted" });
+    }
+  } catch (e) {
+    res.status(500).json({ message: "Error deleting appointment", e });
+  }
+};
+
+// Update Appointment
+export const updateAppointment = async (req, res) => {
+  try {
+    const { status } = req.body;
+    if (!status) {
+      return res.status(400).send("Status required");
+    }
+    const appointment = new Appointment({
+      status,
+    });
+    await Appointment.findByIdAndUpdate(req.params.id, {
+      status,
+    });
+    res.send("Appointment updated");
+  } catch (e) {
+    res.status(500).json({ message: "Error updating appointment", e });
+  }
+};
+
+// Delete Appointment
+export const deleteAppointment = async (req, res) => {
+  try {
+    const appointmentId = req.params.id;
+    const appointment = await Appointment.findByIdAndDelete(appointmentId);
+    if (appointment) {
+      res.status(200).json({ message: "Appointment deleted" });
+    } else {
+      res.status(404).json({ message: "Appointment not found" });
+    }
+  } catch (e) {
+    res.status(500).json({ message: "Error deleting appointment", e });
+  }
+};
