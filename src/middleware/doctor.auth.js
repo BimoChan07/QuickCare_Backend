35ed1e0890e494f9f69dbfe3130fe54b2a441857
BimoChan07 <bmochanb@gmail.com>
2023-06-25 13:42:09 +0545

    doctor authentication added

---
+++ src/middleware/doctor.auth.js
@@ -0,0 +1,27 @@
+import jwt from "jsonwebtoken";
+
+export const idLogin = (req, res, next) => {
+  try {
+    const token = extractTokenFromHeader(req);
+    if (!token) {
+      return res
+        .status(401)
+        .json({ ERR: "you are not authorized to access this route" });
+    }
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    req.doctor = payload;
+
+    return next();
+  } catch (err) {
+    res.status(401).json({
+      ERR: "Something terrible happened during token verification!",
+      message: err,
+    });
+  }
+};
+
+// Function that will extract token from header
+const extractTokenFromHeader = (_AuthRequest) => {
+  const [type, token] = request.headers.authorization?.split(" ") ?? [];
+  return type === "Bearer" ? token : undefined;
+};
